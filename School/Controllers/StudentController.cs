55c2050e504494a9a4ab1d45bd9e9842d7099edd
Your Name <you@example.com>
2023-09-29 15:50:33 -0400

    Student Dashboard

--- School/Controllers/StudentController.cs
+++ School/Controllers/StudentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using School.Models;  // Ensure this using directive is present to access Student model
 
 namespace School.Controllers
 {
@@ -11,7 +12,36 @@ namespace School.Controllers
         // GET: Student
         public ActionResult StudentD()
         {
-            return View();
+            var student = new Student
+            {
+                // Populate student properties here, for example:
+                FirstName = "John",
+                LastName = "Doe",
+                EnrollmentDate = DateTime.Now,
+                Role = "Student",
+                Schedule = new List<ScheduleItem>
+                {
+                    new ScheduleItem
+                    {
+                        Day = "Monday",
+                        Time = "9:00 AM",
+                        Subject = new Subject { Name = "Math" }
+                    },
+                    // ... other schedule items
+                },
+                Assignments = new List<Assignment>
+                {
+                    new Assignment
+                    {
+                        Title = "Assignment 1",
+                        Status = "Incomplete"
+                    },
+                    // ... other assignments
+                }
+                // ... other properties
+            };
+
+            return View(student);
         }
     }
 }
--- School/Models/Student.cs
+++ School/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System;
+using System.Collections.Generic;
 
 namespace School.Models
 {
@@ -9,10 +10,19 @@ namespace School.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+
+        public int Id { get; set; }
         public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public DateTime EnrollmentDate { get; set; }
+
+        public string Role { get ; set; }
         public string classes { get; set; }
         public string grade { get; set; }
+
+        public List<ScheduleItem> Schedule { get; set; }
+
+        public List<Assignment> Assignments { get; set; }
     }
 }
