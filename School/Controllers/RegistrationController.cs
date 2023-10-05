fadd545e04165aa11b47a7b8ed2a8cb4467d5146
imaneAllay <73351438+imaneAllay@users.noreply.github.com>
2023-10-05 14:02:38 -0400

    client side Validation

--- School/Controllers/RegistrationController.cs
+++ School/Controllers/RegistrationController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace School.Controllers
@@ -13,7 +14,7 @@ namespace School.Controllers
     public class RegistrationController : Controller
     {
         private readonly MongoDbContext _context;
-        
+
         public RegistrationController()
         {
             _context = new MongoDbContext();
@@ -21,40 +22,83 @@ namespace School.Controllers
 
         [HttpGet]
         public ActionResult Register()
+
+
         {
-            return View();  
+            return View();
         }
 
         [HttpPost]
-        public ActionResult Register(User user, Teacher profile, Student profile2, Contacts contactInfo)
-        {
-            if(ModelState.IsValid) 
-            {
-                _context.Users.InsertOne(user);
-                var userId = user.Id;
-                var email = user.Email;
-                contactInfo.UserId = userId;
-                contactInfo.Email = email;
+        //public ActionResult Register(User user, Teacher profile, Student profile2, Contacts contactInfo)
+
+        //{
+
+        //    string inputPassword = user.Password;
+        //    string salt = BCrypt.Net.BCrypt.GenerateSalt(12);
+        //    string hashedPassword = BCrypt.Net.BCrypt.HashPassword(inputPassword, salt);
 
 
-                if (user.Role == UserRole.Teacher)
-                {
-                    profile.UserId = userId;
-                    _context.Teachers.InsertOne(profile);
-                }
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Users.InsertOne(user);
+        //        var userId = user.Id;
+        //        var email = user.Email;
+        //        var password = hashedPassword;
+        //        var Salt = salt;
+        //        var role = user.Role;
+        //        contactInfo.UserId = userId;
+        //        contactInfo.Email = email;
 
-                if (user.Role == UserRole.Student)
-                {
-                    profile2.UserId = userId;
-                    _context.Students.InsertOne(profile2);
-                }
-                _context.Contacts.InsertOne(contactInfo);
 
-            }
-            return View(user);
-        }
 
-        [HttpPost]
+        //        if (user.Role == UserRole.Teacher)
+        //        {
+        //            profile.UserId = userId;
+        //            _context.Teachers.InsertOne(profile);
+        //        }
+
+        //        if (user.Role == UserRole.Student)
+        //        {
+        //            profile2.UserId = userId;
+        //            _context.Students.InsertOne(profile2);
+        //        }
+        //        _context.Contacts.InsertOne(contactInfo);
+
+
+
+        //    }
+        //    return View(user);
+        //}
+        //[HttpPost]
+        //public ActionResult Register(User user, Teacher profile, Student profile2, Contacts contactInfo)
+        //{
+        //    if(ModelState.IsValid) 
+        //    {
+        //        _context.Users.InsertOne(user);
+        //        var userId = user.Id;
+        //        var email = user.Email;
+        //        contactInfo.UserId = userId;
+        //        contactInfo.Email = email;
+
+
+        //        if (user.Role == UserRole.Teacher)
+        //        {
+        //            profile.UserId = userId;
+        //            _context.Teachers.InsertOne(profile);
+        //        }
+
+        //        if (user.Role == UserRole.Student)
+        //        {
+        //            profile2.UserId = userId;
+        //            _context.Students.InsertOne(profile2);
+        //        }
+        //        _context.Contacts.InsertOne(contactInfo);
+
+        //    }
+        //    return View(user);
+        //}
+
+        //[HttpPost]
         public ActionResult Next()
         {
             //if (ModelState.IsValid)
