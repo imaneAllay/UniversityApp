d09c1a5f96127182881a5695f9c6033bb125f640
imaneAllay <73351438+imaneAllay@users.noreply.github.com>
2023-10-05 17:49:47 -0400

    Add login validation

--- School/Controllers/RegistrationController.cs
+++ School/Controllers/RegistrationController.cs
@@ -31,42 +31,23 @@ namespace School.Controllers
 
         [HttpPost]
 
-        //public ActionResult Register(User user, Teacher profile, Student profile2, Contacts contactInfo)
-
-        //{
-
-        //    string inputPassword = user.Password;
-        //    string salt = BCrypt.Net.BCrypt.GenerateSalt(12);
-        //    string hashedPassword = BCrypt.Net.BCrypt.HashPassword(inputPassword, salt);
-
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Users.InsertOne(user);
-        //        var userId = user.Id;
-        //        var email = user.Email;
-        //        var password = hashedPassword;
-        //        var Salt = salt;
-        //        var role = user.Role;
-        //        contactInfo.UserId = userId;
-        //        contactInfo.Email = email;
-
+        
         public ActionResult Register(User user, Teacher profile, Student profile2, Contacts contactInfo)
 
         {
 
             string inputPassword = user.Password;
-            //string salt = BCrypt.Net.BCrypt.GenerateSalt(12);
-            //string hashedPassword = BCrypt.Net.BCrypt.HashPassword(inputPassword, salt);
+            string salt = BCrypt.Net.BCrypt.GenerateSalt(12);
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(inputPassword, salt);
 
 
-            if(ModelState.IsValid) 
+            if (ModelState.IsValid) 
             {
                 _context.Users.InsertOne(user);
                 var userId = user.Id;
                 var email = user.Email;
-                //var password = hashedPassword;
-                //var Salt = salt; 
+                var password = hashedPassword;
+                var Salt = salt;
                 var role = user.Role;
                 contactInfo.UserId = userId;
                 contactInfo.Email = email;
@@ -80,99 +61,12 @@ namespace School.Controllers
                 }
 
 
-
-
-               
-
             }
             return View(user);
         }
 
 
-        //        if (user.Role == UserRole.Teacher)
-        //        {
-        //            profile.UserId = userId;
-        //            _context.Teachers.InsertOne(profile);
-        //        }
-
-        //        if (user.Role == UserRole.Student)
-        //        {
-        //            profile2.UserId = userId;
-        //            _context.Students.InsertOne(profile2);
-        //        }
-        //        _context.Contacts.InsertOne(contactInfo);
-
-
-
-        //    }
-        //    return View(user);
-        //}
-        //[HttpPost]
-        //public ActionResult Register(User user, Teacher profile, Student profile2, Contacts contactInfo)
-        //{
-        //    if(ModelState.IsValid) 
-        //    {
-        //        _context.Users.InsertOne(user);
-        //        var userId = user.Id;
-        //        var email = user.Email;
-        //        contactInfo.UserId = userId;
-        //        contactInfo.Email = email;
-
-
-        //        if (user.Role == UserRole.Teacher)
-        //        {
-        //            profile.UserId = userId;
-        //            _context.Teachers.InsertOne(profile);
-        //        }
-
-        //        if (user.Role == UserRole.Student)
-        //        {
-        //            profile2.UserId = userId;
-        //            _context.Students.InsertOne(profile2);
-        //        }
-        //        _context.Contacts.InsertOne(contactInfo);
-
-        //    }
-        //    return View(user);
-        //}
-
-        //[HttpPost]
-        public ActionResult Next()
-        {
-            //if (ModelState.IsValid)
-            //{
-
-
-            //    if (user.Role == UserRole.Student)
-            //    {
-            //        return RedirectToAction("StudentNext");
-            //    }
-            //    else if (user.Role == UserRole.Teacher)
-            //    {
-
-            //        return RedirectToAction("TeacherNext");
-            //    }
-
-            //}
-
-            //return View("Register", user);
-            return Content("");
-        }
-
         
-        [HttpGet]
-        public ActionResult TeacherNext()
-        {
-            
-            return View("TeacherNext");
-        }
-
-        [HttpGet]
-        public ActionResult StudentNext()
-        {
-            
-            return View("StudentNext");
-        }
 
 
 
--- School/Models/User.cs
+++ School/Models/User.cs
@@ -12,9 +12,9 @@ namespace School.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public ObjectId Id { get; set; }
 
-      
+        [Required(ErrorMessage = "Username is required.")]
         public string Username { get; set; }
-
+        [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; }
 
         public string Salt { get; set; }
