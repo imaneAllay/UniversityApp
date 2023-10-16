ee51ddd2a13fd30902863d1ba18e34d658d1d902
imaneAllay <73351438+imaneAllay@users.noreply.github.com>
2023-10-16 16:52:26 -0400

    Validate date of birth

--- School/Controllers/RegistrationController.cs
+++ School/Controllers/RegistrationController.cs
@@ -32,49 +32,74 @@ namespace School.Controllers
         [HttpPost]
         public ActionResult Register( User user)
         {
-            
-            if (!user.ValidateFirstName() )
-            {
-                return Content("fn");
+            // if (!user.ValidateUsername())
+            //{
+            //    return Content("us");
+            //}
+            //else if (!user.ValidatePassword())
+            //{
+            //    return Content("password");
+            //}
+            //else if (!user.ValidateConfirmPassword())
+            //{
+            //    return Content("confirmpassword");
+            //}
+            //else if (!user.ValidateEmail())
+            //{
+            //    return Content("email");
+            //}
+            //else if (!user.ValidatePhoneNumber())
+            //{
+            //    return Content("phone");
+            //}
+            //else if (!user.ValidateFirstName() )
+            //{
+            //    return Content("fn");
 
-            }
-            else if (!user.ValidateLastName())
+            //}
+            //else if (!user.ValidateLastName())
+            //{
+            //    return Content("ln");
+            //}
+          
+            if (user.DateOfBirth == DateTime.MinValue)
             {
-                return Content("ln");
-            }
-            else if (!user.ValidateUsername())
-            {
-                return Content("us");
-            }
-            else if (!user.ValidateEmail())
-            {
-                return Content("email");
-            }
-            else if (!user.ValidatePassword())
-            {
-                return Content("password");
-            }
-            else if (!user.ValidateConfirmPassword())
-            {
-                return Content("confirmpassword");
-            }
-            else if (!user.ValidatePhoneNumber())
-            {
-                return Content("phone");
-            }
-            else if (string.IsNullOrWhiteSpace(user.DateOfBirth))
-                {
                 return Content("DOB");
             }
-            
-            else if (!user.ValidateDeparment())
+            else if (user.DateOfBirth > DateTime.Now)
             {
-                return Content("department");
+                return Content("DOBF");
             }
-            else if (string.IsNullOrWhiteSpace(user.EnrollmentDate))
+            var currentYear = DateTime.Now.Year;
+            var currentMonth = DateTime.Now.Month;
+            var currentDay = DateTime.Now.Day;
+            var minAge = new DateTime (currentYear - 18 , currentMonth , currentDay);
+
+             if (user.DateOfBirth > minAge)
             {
-                return Content("en");
+                return Content("DOBA");
             }
+
+
+
+
+
+
+
+
+            else if (user.DateOfBirth > DateTime.Now)
+            {
+                return Content("DOBF");
+            }
+
+            //else if (!user.ValidateDeparment())
+            //{
+            //    return Content("department");
+            //}
+            //else if (user.EnrollmentDate == DateTime.MinValue)
+            //{
+            //    return Content("en");
+            //}
             else {
                 
                 _context.Users.InsertOne(user);
