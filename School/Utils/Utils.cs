0d1464343ccb82f9afdf05cbcdd43b38e77f1d97
imaneAllay <73351438+imaneAllay@users.noreply.github.com>
2023-10-24 09:50:17 -0400

    Protect Key

--- School/Utils/Utils.cs
+++ School/Utils/Utils.cs
@@ -11,18 +11,20 @@ namespace School.Utils
 {
     public static class Utils
     {
+        private static string publicKey = System.Configuration.ConfigurationManager.AppSettings["PublicKey"];
+
         public static string Encrypt(UserCookie userCookie)
         {
             var jsonStr = Newtonsoft.Json.JsonConvert.SerializeObject(userCookie);
             var cookieContent = Encoding.UTF8.GetBytes(jsonStr);
-            var encryptedValue = Convert.ToBase64String(MachineKey.Protect(cookieContent, "ProtectCookie"));
+            var encryptedValue = Convert.ToBase64String(MachineKey.Protect(cookieContent, publicKey));
             return encryptedValue;
         }
 
         public static UserCookie Decrypt(string getCookie)
         {
             var cookie = Convert.FromBase64String(getCookie);
-            var output = MachineKey.Unprotect(cookie, "ProtectCookie");
+            var output = MachineKey.Unprotect(cookie, publicKey);
             var result = Encoding.UTF8.GetString(output);
             return  Newtonsoft.Json.JsonConvert.DeserializeObject<UserCookie>(result);
           
